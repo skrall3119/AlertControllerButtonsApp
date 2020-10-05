36985ce011565194997b2c57d98b7300de6635c4
skrall3119 <alexjanci@yahoo.com>
2020-10-05 14:44:24 -0500

    init

--- AlertControllerButtonsApp/ViewController.swift
+++ AlertControllerButtonsApp/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelResult: UILabel!
+    @IBAction func buttonTapped(_ sender: Any) {
+        let alert = UIAlertController(title: "Warning", message: "Zombies are loose!", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { action -> Void in
+            self.labelResult.text = "OK"
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: { action -> Void in
+            self.labelResult.text = "Cancel"
+        })
+        let destroyAction = UIAlertAction(title: "Destroy", style: .destructive, handler: {action -> Void in
+            self.labelResult.text = "Destroy"
+        })
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        alert.addAction(destroyAction)
+        self.present(alert, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelResult.numberOfLines = 0
     }
 
 
